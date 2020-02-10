85fa9ad76396401a1bd3654037991756e7a0d226
joshbolt10 <60295471+joshbolt10@users.noreply.github.com>
2020-02-10 11:29:53 -0500

    Repository Objects added
    Pokemon characters added to the repository as objects.

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var repository = [
+  {name: 'Bulbasaur', height: 0.7, types: ['grass', 'water']};
+  {name: 'Pikachu', height: 0.4, types: ['electric', 'flying']};
+  {name: 'Wartortle', height: 1, types: ['steel', 'fire']};
+  {name: 'Ninetales', height: 1.1, types: ['ground', 'ice']};
+];
