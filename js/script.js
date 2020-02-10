05543d7c21282225e8365993743af1fed2b45295
joshbolt10 <60295471+joshbolt10@users.noreply.github.com>
2020-02-10 17:19:35 -0500

    Loop added
    Added loop to check for repository height.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,35 @@
-var repository = [
-  {name: 'Bulbasaur', height: 0.7, types: ['grass', 'water']};
-  {name: 'Pikachu', height: 0.4, types: ['electric', 'flying']};
-  {name: 'Wartortle', height: 1, types: ['steel', 'fire']};
-  {name: 'Ninetales', height: 1.1, types: ['ground', 'ice']};
-];
+var pokemonRepository = (function () {
+  var repository = [
+    {
+      name: 'Bulbasaur',
+      height: 0.7,
+      types: ['grass', 'water']
+    },
+    {
+      name: 'Pikachu',
+      height: 0.4,
+      types: ['electric', 'flying']
+    },
+    {
+      name: 'Wartortle',
+      height: 1,
+      types: ['steel', 'fire']
+    },
+    {
+      name: 'Ninetales',
+      height: 1.1,
+      types: ['ground', 'ice']
+    },
+  ];
+
+for (i = 0; i < repository.length; i++) {
+  if (repository.height <= 1) {
+    document.write(
+      repository.name + repository.height
+    );
+  } else {
+    document.write(
+      repository.name + repository.height + "Wow, that's big!"
+    );
+  }
+}
